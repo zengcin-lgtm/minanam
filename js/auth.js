c99af8d87e3f202a260cc6de9752b2f64a5cd9dc
Foday Ka'ti <zengcin@gmail.com>
2026-03-13 16:59:30 +0800

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -6,7 +6,7 @@
 class AuthSystemClass {
     constructor() {
         // ★★★ 請務必將這裡換成您剛剛重新發布的 Google Apps Script 網址 ★★★
-        this.API_URL = "https://script.google.com/macros/s/AKfycbxxxx_Your_API_URL_xxxx/exec"; 
+        this.API_URL = "https://script.google.com/macros/s/AKfycbzihyxv1NyH1IgBF8kWBVXLNE1-FVETTYxy-6Y49te7DTULQj5cZeDe6BLtBadEvk44/exec"; 
         
         this.currentUser = null;
         this.init();
